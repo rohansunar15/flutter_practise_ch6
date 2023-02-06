c53dcabe1627d71424b3629dceefc8cfee50ea60
rohan <rohansunar12@gmail.com>
2023-02-06 13:18:01 +0530

    rows and column

--- lib/Home.dart
+++ lib/Home.dart
@@ -73,61 +73,20 @@ class ContainerWithBoxContainetWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Container(
-          height: 100.0,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(100.0),
-              bottomRight: Radius.circular(10.0),
-            ),
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.white,
-                Colors.lightGreenAccent,
-              ]
-            ),
-            boxShadow: [
-              BoxShadow(
-              color: Colors.lightGreen,
-              blurRadius: 10.0,
-              offset: Offset(0.0,10.0),
-               ),
-            ]
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Text('Row 1'),
+            Padding(padding: EdgeInsets.all(16.0),),
+            Text('Row 2'),
+            Padding(padding: EdgeInsets.all(16.0),),
+            Text('Row 3'),
+            ],
           ),
-          child: Center(
-            child: RichText(
-              text: TextSpan(
-              text: 'Rohan ',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 25.0,
-                  decorationColor: Colors.lightBlue,
-                  decoration: TextDecoration.underline,
-                  decorationStyle: TextDecorationStyle.dashed,
-                ),
-                children:<TextSpan>[
-                  TextSpan(
-                    text: 'Sunar',
-
-                  ),
-                  TextSpan(
-                    text: '  Mobile',
-                    style: TextStyle(
-                      color: Colors.redAccent,
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ]
-            ),
-            ),
-            ),
-          ),
-      ],
-    );
+        ],
+      );
+    }
   }
-}
+
