e418d433ea5867bfbc5c0b9f55bf81ea851c19e5
rohan <rohansunar12@gmail.com>
2023-02-10 16:33:41 +0530

    button

--- lib/Home.dart
+++ lib/Home.dart
@@ -27,76 +27,156 @@ class _HomeState extends State<Home> {
               icon: Icon(Icons.more_vert) ,
               onPressed: (){},
             ),
+
           ],
             flexibleSpace: const SafeArea(
               child: Icon(
                 Icons.photo_camera,
                 size: 50.0,
                 color: Colors.black,
-                ),
-              ),
-          /*bottom: PreferredSize(
-            child: Container(
-              color: Colors.deepPurpleAccent,
-              height: 75.0,
-              width: double.infinity,
-              child:Center(
-                child:Text('Bottom'),
               ),
             ),
-            preferredSize: Size.fromHeight(75.0),
-          ),*/
-          
+          bottom: PopupMenuButtonWidget(),
+        ),
+
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.lightGreen,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Icon(Icons.pause),
+              Icon(Icons.stop),
+              Icon(Icons.access_time_filled_rounded),
+              Padding(
+                padding: EdgeInsets.all(32.0),
+              ),
+            ],
+          ),
         ),
 
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                const ContainerWithBoxContainetWidget(),
-                ],
-            ),
+          child: Column(
+            children: <Widget>[
+              const ButtonsWidget(),
+              Divider(),
+              const ButtonBarWidget(),
+              Divider(),
+            ],
           ),
         ),
       ),
-
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed:(){},
+        label: Text('Play'),
+        icon: Icon(Icons.play_arrow_rounded),
+      ),
     );
   }
 }
 
-class ContainerWithBoxContainetWidget extends StatelessWidget {
-  const ContainerWithBoxContainetWidget({Key? key}) : super(key: key);
+class PopupMenuButtonWidget extends StatelessWidget implements PreferredSizeWidget{
+  const PopupMenuButtonWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightGreen.shade100,
+      height:preferredSize.height,
+      width: double.infinity,
+      child: Center(
+        child: PopupMenuButton<TodoMenuItem>(
+          icon: Icon(Icons.view_list),
+          onSelected: ((valueSelected){
+            print('valueSelected: ${valueSelected.title}');
+          }),
+          itemBuilder: (BuildContext context){
+            return foodMenuList.map((TodoMenuItem todoMenuItem) {
+              return PopupMenuItem<TodoMenuItem>(
+                value: todoMenuItem,
+                child: Row(
+                  children: <Widget>[
+                    Icon(todoMenuItem.icon.icon),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+
+                    ),
+                    Text(todoMenuItem.title),
+                  ],
+                ),
+              );
+            }).toList();
+          },
+        ),
+      ),
+    )
+  }
+  @override
+  Size get preferredSize => Size.fromHeight(75.0);
+}
+
+
+
+
+class TodoMenuItem {
+  final String title;
+  final Icon icon;
+
+  TodoMenuItem({required this.title, required this.icon});
+}
+
+List<TodoMenuItem> foodMenuList=[
+  TodoMenuItem(title: 'Fast food', icon: Icon(Icons.fastfood)),
+  TodoMenuItem(title: 'Remind me', icon: Icon(Icons.add_alarm)),
+  TodoMenuItem(title: 'Flight', icon: Icon(Icons.flight)),
+];
+
+class ButtonsWidget extends StatelessWidget {
+  const ButtonsWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
+        Row(
           children: <Widget>[
-            Text('Column 1'),
-            Divider(),
-            Text('Column 2'),
-            Divider(),
-            Text('Column 3'),
-            Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('Row 1'),
-                Text('Row 2'),
-                Text('Row 3'),
-              ],
+            Padding(padding: EdgeInsets.all(16.0)),
 
-            ),
           ],
         ),
-        ],
-      );
-    }
+      ],
+    );
   }
+}
 
+
+class ButtonBarWidget extends StatelessWidget {
+  const ButtonBarWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white70,
+      child: ButtonBar(
+        alignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          IconButton(
+              onPressed: (){},
+              icon: Icon(Icons.map),
+          ), 
+          IconButton(
+            onPressed: (){}, 
+            icon: Icon(Icons.airport_shuttle),
+          ),
+          IconButton(
+              onPressed: (){}, 
+              icon: Icon(Icons.brush),
+            highlightColor: Colors.purple,
+          ),
+        ],
+      ),
+    );
+  }
+}
